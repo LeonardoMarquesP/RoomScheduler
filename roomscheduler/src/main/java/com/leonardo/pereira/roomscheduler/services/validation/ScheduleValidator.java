c1272128a40087d542b478b10e5f9fb9c10e30ed
LeonardoMarquesP <leonardom.pereira@hotmail.com>
2024-10-22 18:01:54 -0300

    Schedule Validator Part2

--- roomscheduler/src/main/java/com/leonardo/pereira/roomscheduler/services/validation/ScheduleValidator.java
+++ roomscheduler/src/main/java/com/leonardo/pereira/roomscheduler/services/validation/ScheduleValidator.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.leonardo.pereira.roomscheduler.entities.Schedule;
-import com.leonardo.pereira.roomscheduler.entities.dto.ScheduleDTO;
 import com.leonardo.pereira.roomscheduler.entities.dto.ScheduleInsertUpdateDTO;
 import com.leonardo.pereira.roomscheduler.infra.FieldMessage;
 import com.leonardo.pereira.roomscheduler.repositories.ScheduleRepository;
@@ -27,33 +26,26 @@ public class ScheduleValidator implements ConstraintValidator<ScheduleValid, Sch
 	
 	@Override
 	public boolean isValid(ScheduleInsertUpdateDTO dto, ConstraintValidatorContext context) {
-		//Lista de Erros
 		List<FieldMessage> list = new ArrayList<>();
 				
-		// Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
-				
-		//Teste 1 - Se a data esta no formato certo
-		//Schedule schedule2 = repository.findByScheduledDate(dto.getScheduledDate());
-		
-		
-		//Teste 2 - Se a data não está após 90 dias no futuro
-		//Schedule schedule = repository.findByScheduledDate(dto.getScheduledDate());
-		//LocalDate date = schedule.getScheduledDate();
+		//Teste 1 - Se a data não está após 90 dias no futuro
 		LocalDate dateS = dto.getScheduledDate();
-		
-		// Data atual
         LocalDate today = LocalDate.now();
-        // Data máxima (90 dias no futuro)
         LocalDate maxDate = today.plusDays(90);
-        
-        // Verifica se a data está no intervalo entre hoje e até 90 dias no futuro
         if (dateS.isBefore(today) || dateS.isAfter(maxDate)) {
-            //throw new IllegalArgumentException("A data agendada deve estar entre hoje e 90 dias no futuro.");
             list.add(new FieldMessage("scheduledDate", "Scheduled Date is invalid"));
+        }	
         
+        //Teste 2
+        Schedule dateS2 = repository.findByScheduledDate(dto.getScheduledDate());
+        if (dateS2 != null) {
+        	list.add(new FieldMessage("scheduledDate", "Scheduled Date already exists"));
         }
-				
-				
+        
+        
+        
+        
+        
 		//Pega a lista fieldMessage e insere no beansvalidation os erros
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
@@ -62,5 +54,9 @@ public class ScheduleValidator implements ConstraintValidator<ScheduleValid, Sch
 		}
 		return list.isEmpty();
 	}
+	
+	
+	
+	
 
 }
